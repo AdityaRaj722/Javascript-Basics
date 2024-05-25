dd25b5272c40cbae6701fcc466fc8bad2865ef28
AdityaRaj722 <aditya0722004@gmail.com>
2024-05-25 21:36:11 +0530

    variable

---
+++ variandConst/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=144559
+let accountEmail="aditya0722004@gmail.com"
+var accountPassword="12345"
+accountCity="Bihar"
+ 
+// accountId=313546 // not allowed
+
+accountEmail="safkjjbf@sdh.com"
+accountPassword="543265"
+accountCity="Delhi"
+
+console.log([accountEmail,accountId,accountPassword,accountCity])
