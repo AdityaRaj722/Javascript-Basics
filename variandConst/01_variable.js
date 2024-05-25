2cdb5c46e8cbc4e6909408d043a89d371ea8acf0
AdityaRaj722 <aditya0722004@gmail.com>
2024-05-25 21:41:58 +0530

    variable

--- variandConst/01_variable.js
+++ variandConst/01_variable.js
@@ -2,11 +2,11 @@ const accountId=144559
 let accountEmail="aditya0722004@gmail.com"
 var accountPassword="12345"
 accountCity="Bihar"
- 
+let dslfkf;
 // accountId=313546 // not allowed
 
 accountEmail="safkjjbf@sdh.com"
 accountPassword="543265"
 accountCity="Delhi"
 
-console.log([accountEmail,accountId,accountPassword,accountCity])
+console.log([dslfkf,accountEmail,accountId,accountPassword,accountCity])
