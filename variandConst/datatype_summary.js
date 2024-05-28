94f81c9e2e1c512ac995e06d429bb248fdd37501
AdityaRaj722 <aditya0722004@gmail.com>
2024-05-28 20:33:17 +0530

    summary

---
+++ variandConst/datatype_summary.js
@@ -0,0 +1,22 @@
+const score=100
+const scoreValue=100.3
+const isLogged=false
+const temp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+const bigNum=46541231645n
+console.log(id===anotherId)
+
+
+
+const heros=["saktiman","naagraj","karana"];
+let myObj={
+    name:"Adi",
+    age:20,
+
+}
+
+const myFunction=function(){
+    console.log("Hello")
+}
