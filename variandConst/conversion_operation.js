ae235b2bc3a5189c9084bcbdca1b1901c49feda1
AdityaRaj722 <aditya0722004@gmail.com>
2024-05-28 19:28:22 +0530

    datatypeconversion

---
+++ variandConst/conversion_operation.js
@@ -0,0 +1,10 @@
+let score=33
+console.log(typeof score)
+ console.log(typeof {score} )
+
+ let a=1;
+ let booleanA=Boolean(a)
+ console.log(booleanA)
+ let number=55
+ let stringNum=String(number)
+ console.log(typeof stringNum)
