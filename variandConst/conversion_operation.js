91573f933e03212b3b430a33d3a1d27454ca4f77
AdityaRaj722 <aditya0722004@gmail.com>
2024-05-28 19:43:42 +0530

    update

--- variandConst/conversion_operation.js
+++ variandConst/conversion_operation.js
@@ -1,10 +1,28 @@
-let score=33
-console.log(typeof score)
- console.log(typeof {score} )
+// let score=33
+// console.log(typeof score)
+//  console.log(typeof {score} )
 
- let a=1;
- let booleanA=Boolean(a)
- console.log(booleanA)
- let number=55
- let stringNum=String(number)
- console.log(typeof stringNum)
+//  let a=1;
+//  let booleanA=Boolean(a)
+//  console.log(booleanA)
+//  let number=55
+//  let stringNum=String(number)
+//  console.log(typeof stringNum)
+// ********************OPERATION**************//
+let val=3
+let negValue=-val
+console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%2)
+
+let str1="Hello"
+let str2=" Adi"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
