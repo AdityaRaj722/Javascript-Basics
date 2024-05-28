b4c183cb68c886baa51edabe87b9ae9b6fa3b66c
AdityaRaj722 <aditya0722004@gmail.com>
2024-05-28 19:53:42 +0530

    update

--- variandConst/tempCodeRunnerFile.js
+++ variandConst/tempCodeRunnerFile.js
@@ -1,9 +1,5 @@
+onsole.log("2">1)
 
-
-let str1="Hello"
-let str2=" Adi"
-let str3=str1+str2
-console.log(str3)
-
-console.log("1"+2)
-console.log(1+"2")
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
